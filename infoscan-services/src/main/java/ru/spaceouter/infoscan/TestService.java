71ccc879436265683592c9c3adb5e78ed6907a9e
prog_ja <script.bot@mail.ru>
2019-04-09 23:25:05 +0400

    Initial commit

---
+++ infoscan-services/src/main/java/ru/spaceouter/infoscan/TestService.java
@@ -0,0 +1,12 @@
+package ru.spaceouter.infoscan;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+    public String getText(){
+        return "Hello bitches";
+    }
+
+}
---
+++ infoscan-view/infoscan-view-controller/src/main/java/ru/spaceouter/infoscan/TestController.java
@@ -0,0 +1,23 @@
+package ru.spaceouter.infoscan;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestController {
+
+    private final TestService testService;
+
+    @Autowired
+    public TestController(TestService testService) {
+        this.testService = testService;
+    }
+
+    @GetMapping("/")
+    public ModelAndView test(){
+        return new ModelAndView("root", "property", testService.getText());
+    }
+
+}
